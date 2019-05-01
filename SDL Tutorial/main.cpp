09c13d9517f47c52601059e6fd543aceba1d9944
Gabriel Teodósio <gabrieltmm@gmail.com>
2019-05-01 20:05:34 -0300

    Initial Commit

---
+++ SDL Tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL Tutorial
+//
+//  Created by Gabriel T Marques on 01/05/19.
+//  Copyright © 2019 Gabriel T Marques. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
